c37ac37c537efa4fdb3ed9d9068c1ac2e4895239
Karol Nowak <k.nowak021@gmail.com>
2020-12-28 18:10:49 +0100

    feat (download) add unnecessary bytes trimming

--- pkg/grpc/download-server.go
+++ pkg/grpc/download-server.go
@@ -26,25 +26,70 @@ func (s *downloadServer) DownloadFile(req *downloadpb.FileDownloadRequest, strea
 		return status.Errorf(codes.NotFound, "file not foud")
 	}
 
+	var bytesStack [][]byte
+
 	for {
+		//get bytes from file
 		bytes, err := s.ds.ReadBytes()
 		if err == io.EOF {
+			// if end of file trim 0 bytes and return byte slice without them
+			trimmedBytes := trimBytes(bytesStack[0])
+
+			req := byteRequest(trimmedBytes)
+
+			stream.Send(req)
 			break
 		}
+
 		if err != nil {
 			return status.Errorf(codes.Internal, "error while reading file")
 		}
 
-		res := &downloadpb.FileDownloadResponse{
-			Body: &downloadpb.FileDownloadBody{
-				Bytes: bytes,
-			},
-		}
+		bytesStack = append(bytesStack, bytes)
 
-		stream.Send(res)
+		//byte slices are pushed to slice and if len is bigger than 2 request is send
+		if len(bytesStack) > 1 {
+			//bytes at position 0 are sended
+			req := byteRequest(bytesStack[0])
+
+			stream.Send(req)
+
+			//sended bytes are removed from bytesStack
+			bytesStack = bytesStack[1:]
+		}
 	}
 
-	// go s.ds.RecordDownloadFile(userID)
-
 	return nil
+}
+
+func trimBytes(bytes []byte) []byte {
+	var firstZeroByteIndex int
+	firstZeroWasHitted := false
+
+	for i, singleByte := range bytes {
+		if singleByte == 0 && !firstZeroWasHitted {
+			firstZeroByteIndex = i
+			firstZeroWasHitted = true
+		}
+		if singleByte != 0 {
+			firstZeroByteIndex = -1
+			firstZeroWasHitted = false
+		}
+	}
+
+	if firstZeroByteIndex == -1 {
+		firstZeroByteIndex = len(bytes)
+	}
+
+	bytes = bytes[:firstZeroByteIndex]
+
+	return bytes
+}
+
+func byteRequest(bytes []byte) *downloadpb.FileDownloadResponse {
+	return &downloadpb.FileDownloadResponse{
+		Body: &downloadpb.FileDownloadBody{
+			Bytes: bytes,
+		},
+	}
 }
